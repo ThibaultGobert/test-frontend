6d20c8788912587078a522333aafcf86836e7773
Unknown <simondegheselle@hotmail.com>
2017-08-21 11:59:16 +0200

    Styled header

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -4,8 +4,20 @@ import { Link, IndexLink } from 'react-router';
 const Header = () => {
   return (
     <nav>
-      <IndexLink to="/" activeClassName="active">Home</IndexLink>
-      <Link to="/manage" activeClassName="active">Manage</Link>
+      <div className="ui attached stackable menu">
+        <div className="ui container">
+          <IndexLink to="/" activeClassName="active" className="item"><i className="home icon"></i> Home</IndexLink>
+          <Link to="/manage/lessons" className="item" activeClassName="active"><i className="grid layout icon"></i> Lessons</Link>
+
+          <div className="right item">
+            <div className="ui input"><input type="text" placeholder="Search..." />
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+
     </nav>
   );
 };
