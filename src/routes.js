0dc168bcfa3d0f60a534c5845931cc452aad726e
Unknown <simondegheselle@hotmail.com>
2017-08-21 11:58:56 +0200

    route to LessonsPage

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
-import ManagePage from "./components/manage/ManagePage";
+import LessonsPage from "./components/manage/LessonsPage";
 
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
-    <Route path="manage" component={ManagePage} />
+    <Route path="manage/lessons" component={LessonsPage} />
   </Route>
 );
 
