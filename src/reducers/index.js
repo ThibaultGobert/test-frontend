e603d2a3279cae1639b1f665f6a0518a6857ae05
Unknown <simondegheselle@hotmail.com>
2017-08-25 15:42:46 +0200

    added initialState + ajax status reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
 // Set up your root reducer here...
- import { combineReducers } from 'redux';
+import { combineReducers } from 'redux';
+import ajaxCallsInProgress from 'ajaxStatus';
 
 const rootReducer = combineReducers({
-
+  ajaxCallsInProgress
 });
 
  export default rootReducer;
