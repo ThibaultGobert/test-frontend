34c26c92336fc871614dfc5336bcf9b75cb7eafd
Unknown <simondegheselle@hotmail.com>
2017-09-06 23:01:03 +0200

    relative paths in production build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -20,8 +20,8 @@ export default {
   entry: path.resolve(__dirname, 'src/index'),
   target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
   output: {
-    path: path.resolve(__dirname, '/dist-server/dist'),
-    publicPath: '/',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: './',
     filename: '[name].[chunkhash].js'
   },
   plugins: [
