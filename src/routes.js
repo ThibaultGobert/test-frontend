a910b5e6e544b122ba3f6f690fa02dbb0c3ce925
Unknown <simondegheselle@hotmail.com>
2017-09-08 15:48:14 +0200

    routes include index route for home page (temp)

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import TeacherOverviewPage from "./components/teacherprofile/ClassGroupsPage";
 import ClassListPage from "./components/teacherprofile/ClassListPage";
@@ -11,24 +11,31 @@ import ChallengesPage from "./components/studentprofile/ChallengesPage";
 import HomeworkPage from "./components/studentprofile/HomeworkPage";
 import SlideViewerPage from "./components/shared/slideviewer/SlideViewerPage";
 import CalendarPage from "./components/teacherprofile/CalendarPage";
+import HomePage from "./components/home/HomePage";
+import CalendarDetailPage from "./components/teacherprofile/CalendarDetailPage";
 
 const routes = (store) => {
   return (
     <Route path="/" component={App}>
+      <IndexRoute component={HomePage}/>
+
       <Route path="/teacherprofile" onEnter={requireAuth(store)}>
         <Route path="/teacherprofile/overview" component={TeacherOverviewPage}/>
         <Route path="/teacherprofile/calendar" component={CalendarPage}/>
+        <Route path="/teacherprofile/calendar/:eventId" component={CalendarDetailPage}/>
         <Route path="/teacherprofile/classlist/:id" component={ClassListPage}/>
       </Route>
+
       <Route path="/studentprofile" onEnter={requireAuth(store)}>
         <Route path="/studentprofile/challenges" component={ChallengesPage}/>
         <Route path="/studentprofile/clan" component={ClanPage}/>
         <Route path="/studentprofile/homework" component={HomeworkPage}/>
-
       </Route>
+
       <Route path="/editorprofile" onEnter={requireAuth(store)}>
         <Route path="/editorprofile/overview" component={EditorOverviewPage}/>
       </Route>
+
       <Route path="/slideviewer" onEnter={requireAuth(store)}>
         <Route path="/slideviewer/:id" component={SlideViewerPage}/>
       </Route>
@@ -61,25 +68,22 @@ const requireNoAuth = (store) => {
         case "STUDENT":
           path = {
             pathname: '/studentprofile/homework',
-            state: { nextPathname: location.location.pathname }
+            state: {nextPathname: location.location.pathname}
           };
           break;
         case "TEACHER":
           path = {
             pathname: '/teacherprofile/overview',
-            state: { nextPathname: location.location.pathname }
+            state: {nextPathname: location.location.pathname}
           };
           break;
-        case "ADMIN":
+        case "EDITOR":
           path = {
             pathname: '/editorprofile/overview',
-            state: { nextPathname: location.location.pathname }
+            state: {nextPathname: location.location.pathname}
           };
           break;
-        default:
-          break;
       }
-
       replace(path);
     }
   };
