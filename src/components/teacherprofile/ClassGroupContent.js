dba99b7c7069280be4ebbeab4de3cc2d6c43a96a
Demian Dekoninck <demian@live.be>
2017-11-18 19:17:37 +0100

    prettier

--- src/components/teacherprofile/ClassGroupContent.js
+++ src/components/teacherprofile/ClassGroupContent.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import {Link} from 'react-router';
+import { Link } from 'react-router';
 import PropTypes from 'prop-types';
 import { Grid, Segment, Button } from 'semantic-ui-react';
 import renderHtml from 'react-render-html';
-import ContactList from "../shared/ContactList";
+import ContactList from '../shared/ContactList';
 import _ from 'lodash';
 
-const ClassGroupContent = ({content}) => {
-  let classListKey = "classlist" + content.id;
-  let feedbackKey = "feedback" + content.id;
+const ClassGroupContent = ({ content }) => {
+  let classListKey = 'classlist' + content.id;
+  let feedbackKey = 'feedback' + content.id;
   // TODO Simon: use contact list for headteachers
   return (
     <Grid className="class-group-details">
@@ -17,32 +17,36 @@ const ClassGroupContent = ({content}) => {
           <Segment vertical>
             <h3>Hoofdlesgever</h3>
             <Grid>
-              <Grid.Row >
-                <Grid.Column width={5}>
-                  {content.headteacher.name}
-                </Grid.Column>
-                <Grid.Column width={7}>
-                  {content.headteacher.email}
-                </Grid.Column>
-                <Grid.Column width={4}>
-                  {content.headteacher.cellphone}
-                </Grid.Column>
+              <Grid.Row>
+                <Grid.Column width={5}>{content.headteacher.name}</Grid.Column>
+                <Grid.Column width={7}>{content.headteacher.email}</Grid.Column>
+                <Grid.Column width={4}>{content.headteacher.cellphone}</Grid.Column>
               </Grid.Row>
             </Grid>
           </Segment>
-          { !_.isEmpty(content.assistants) && <Segment vertical>
-            <h3>Assistent-lesgevers</h3>
-            <ContactList contacts={content.assistants}/>
-          </Segment>}
+          {!_.isEmpty(content.assistants) && (
+            <Segment vertical>
+              <h3>Assistent-lesgevers</h3>
+              <ContactList contacts={content.assistants} />
+            </Segment>
+          )}
         </Grid.Column>
         <Grid.Column width={6}>
           <Segment vertical>
             <h3>Locatie</h3>
-            <p><strong>Organisatie</strong> {content.location.organisation}</p>
-            <p><strong>Adres</strong> {content.location.address}</p>
-            <p><strong>Lokaal</strong> {content.location.room}</p>
-            <p><strong>Opmerkingen</strong> {content.location.roomremark && renderHtml(content.location.roomremark)}</p>
-
+            <p>
+              <strong>Organisatie</strong> {content.location.organisation}
+            </p>
+            <p>
+              <strong>Adres</strong> {content.location.address}
+            </p>
+            <p>
+              <strong>Lokaal</strong> {content.location.room}
+            </p>
+            <p>
+              <strong>Opmerkingen</strong>{' '}
+              {content.location.roomremark && renderHtml(content.location.roomremark)}
+            </p>
           </Segment>
         </Grid.Column>
         <Grid.Column width={2}>
@@ -53,7 +57,7 @@ const ClassGroupContent = ({content}) => {
             <Button>Aanwezigheden</Button>
           </Link>
         </Grid.Column>
-     </Grid.Row>
+      </Grid.Row>
     </Grid>
   );
 };
