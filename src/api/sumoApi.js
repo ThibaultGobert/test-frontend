ec42e891c6c8be5be159678e50a96dc414120645
Simon De Gheselle <simondegheselle@hotmail.com>
2017-11-30 11:07:22 +0100

    add sumo endpoint

--- src/api/sumoApi.js
+++ src/api/sumoApi.js
@@ -1,36 +1,22 @@
-/* global window */
 import axios from 'axios';
-import { lpfUrl } from './baseUrl';
-
-export const getUser = () => {
-  return JSON.parse(window.localStorage.getItem('api.user'));
-};
-
-export const setUser = (newUser) => {
-  window.localStorage.setItem('api.user', JSON.stringify(newUser));
-};
-
-export const removeUser = () => {
-  window.localStorage.removeItem('api.user');
-  window.location = "/";
-};
+import { sumoUrl } from './baseUrl';
 
 const request = (endpoint, { headers = {}, body, ...otherOptions }, method) => {
   const allHeaders = {
     ...headers,
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Origin': '*'
   };
   if (getUser() !== null) {
     allHeaders['x-token'] = getUser().token;
   }
 
-  return axios(`${lpfUrl}${endpoint}`, {
+  return axios(`${sumoUrl}${endpoint}`, {
     ...otherOptions,
     headers: allHeaders,
     timeout: 5000,
     data: body ? JSON.stringify(body) : undefined,
-    method,
+    method
   }).then(response => response.data);
 };
 
@@ -43,7 +29,7 @@ const api = {
   },
   put(endpoint, options = {}) {
     return request(endpoint, options, 'put');
-  },
+  }
 };
 
 export default api;
