de37e36737ebe6f16c2938ae97720faf61ae1762
Simon De Gheselle <simondegheselle@hotmail.com>
2017-12-05 15:14:56 +0100

    notes selector filter on object id

--- src/selectors/notes.js
+++ src/selectors/notes.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 export const getNotesByStudentId = (state, studentId) => {
-  return [];
+  return _.values(state.notes.data).filter(note => note.object_id === studentId);
 };
