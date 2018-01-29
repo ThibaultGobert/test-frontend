ecce98d37c4340a7af9e6a2a90086aa8cc942d02
Unknown <simondegheselle@hotmail.com>
2018-01-29 10:37:51 +0100

    isStudent selector

--- src/components/shared/SlideViewer/index.js
+++ src/components/shared/SlideViewer/index.js
@@ -4,14 +4,14 @@ import SlideViewerContainer from './SlideViewerContainer';
 import * as userRoles from '../../../constants/roles';
 import { fetchLessonSlidesStart, fetchLessonSlidesSuccess, fetchLessonSlidesError } from '../../../actions/slides';
 import mapActionCreatorsToProps from '../../../functions/mapActionCreatorsToProps';
-import { getSlidesByLessonId, getLessonById } from '../../../selectors';
+import { getSlidesByLessonId, getLessonById, isStudent as isStudentSelector } from '../../../selectors';
 
 function mapStateToProps(state, ownProps) {
   const lessonId = ownProps.match.params.id;
   const slideType = ownProps.match.params.type;
   const slides = getSlidesByLessonId(state, lessonId);
   const lesson = getLessonById(state, lessonId);
-  const isStudent = isStudent(state);
+  const isStudent = isStudentSelector(state);
 
   return {
     slides,
