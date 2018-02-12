8bc532ba9faeeabe44a860310e5ba6a944cbbe83
simondegheselle <simondegheselle@hotmail.com>
2018-02-12 18:30:57 +0100

    Info page submenu setup

---
+++ src/components/Teacher/Info/FeedbackLeerlingenPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const AfwezighedenPage = () => {
+  return (
+    <div className="AfwezighedenPage">
+      <h1>Feedback over de leerling</h1>
+      <h2>Afwezigheid melden</h2>
+      <p>
+        We werken momenteel aan een alternatieve manier om feedback over de leerlingen op een
+        gestructureerde en envoudige manier te laten invullen.
+      </p>
+      <p>
+        De hoofdleraar dient ten uiterste bij de 10de les feedback te geven over alle leerlingen via
+        zijn lerarenprofiel. Uiteraard mogen de assistenten hier ook extra voor ingeschakeld worden,
+        zeker bij grote klasgroepen, maar enkel de leraar kan deze feedback invullen in zijn
+        lerarenprofiel.{' '}
+      </p>
+      <p>Dit invullen is essentieel en is <u>een verplichte taak als hoofdleraar.</u></p>
+    </div>
+  );
+};
+
+export default AfwezighedenPage;
