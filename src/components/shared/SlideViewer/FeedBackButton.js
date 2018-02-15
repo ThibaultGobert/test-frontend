67b66165e82d13d05878b5329223d9905e21e6c3
Maarten Raes <maarten.raes1@gmail.com>
2018-02-15 21:43:36 +0100

    Added button for students for survey
    Not tested yet

--- src/components/shared/SlideViewer/FeedBackButton.js
+++ src/components/shared/SlideViewer/FeedBackButton.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button } from 'semantic-ui-react';
+import * as roles from '../../../constants/roles';
 
 const getRefactoredLessonType = (lesson) => {
   switch (lesson.slideType) {
@@ -16,15 +17,31 @@ const getRefactoredLessonType = (lesson) => {
   }
 };
 
+const getHashFromName = (name) => {
+  let hash;
+  for (let i = 0; i < name.length; i += 1) {
+    hash += `${Number(name.charCodeAt(i)) * name.length}-`;
+  }
+  hash = hash.slice(0, -1);
+  return hash;
+};
+
 const generateUrl = (slide, loggedInUser, lesson) => {
-  let formUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSe5cqrRK5t0m_oBgiW7vavymXHiVOi4dHkwDhmZppxiQoqOtA/viewform?usp=pp_url&entry.1028113136&entry.2145939336=name&entry.410557796=slideId&entry.2130843617=type&entry.1966597930=clan&entry.730223016=level';
-  const slideId = `${lesson.id}_${slide.order}`;
-  formUrl = formUrl
-    .replace('name', loggedInUser.fullname)
-    .replace('slideId', slideId)
-    .replace('type', getRefactoredLessonType(lesson))
-    .replace('clan', lesson.group)
-    .replace('level', lesson.level);
+  let formUrl;
+  if (loggedInUser.role === roles.TEACHER_ROLE) {
+    formUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSe5cqrRK5t0m_oBgiW7vavymXHiVOi4dHkwDhmZppxiQoqOtA/viewform?usp=pp_url&entry.1028113136&entry.2145939336=name&entry.410557796=slideId&entry.2130843617=type&entry.1966597930=clan&entry.730223016=level';
+    const slideId = `${lesson.id}_${slide.order}`;
+    formUrl = formUrl
+      .replace('name', loggedInUser.fullname)
+      .replace('slideId', slideId)
+      .replace('type', getRefactoredLessonType(lesson))
+      .replace('clan', lesson.group)
+      .replace('level', lesson.level);
+  } else {
+    formUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSfJuKns1Y-fEepHUw5s7QLTvMu-FooShKnh41jOD1LqLZIouw/viewform?usp=pp_url&entry.1102385648=name';
+    formUrl = formUrl.replace('name', getHashFromName(loggedInUser.fullname));
+  }
+
   return formUrl;
 };
 
