de93f34706757787e30c4ffedc7fb2bff8994cd1
simondegheselle <simondegheselle@hotmail.com>
2018-02-21 11:22:27 +0100

    extract classlist columns

---
+++ src/components/Teacher/ClassList/ClassListColumns.js
@@ -0,0 +1,66 @@
+import noUserIcon from '../../../assets/images/placeholders/no-user.png';
+
+const columns = [
+  {
+    defaults: noUserIcon,
+    display: '',
+    key: 'avatarurlmedium',
+    type: 'string',
+  },
+  {
+    defaults: '',
+    display: 'Naam',
+    key: 'name',
+    type: 'string',
+  },
+  {
+    defaults: '',
+    display: 'Geboortedatum',
+    key: 'birthdate',
+    type: 'number',
+  },
+  {
+    defaults: '',
+    display: 'Leerjaar',
+    key: 'grade',
+    type: 'number',
+  },
+  {
+    defaults: '',
+    display: 'Naam ouder',
+    key: 'parent_name',
+    type: 'string',
+  },
+  {
+    defaults: '',
+    display: 'Contact ouder',
+    key: 'parent_contact',
+    type: 'string',
+  },
+  {
+    defaults: '',
+    display: 'Leerplatform username',
+    key: 'usernames_platform',
+    type: 'string',
+  },
+  {
+    defaults: '',
+    display: 'Leerplatform hint',
+    key: 'password_hint',
+    type: 'string',
+  },
+  {
+    defaults: '',
+    display: 'Scratch username',
+    key: 'scratchusername',
+    type: 'string',
+  },
+  {
+    defaults: '',
+    display: 'Scratch paswoord',
+    key: 'scratchpassword',
+    type: 'string',
+  },
+];
+
+export default columns;
--- src/components/Teacher/ClassList/ClassListContainer.js
+++ src/components/Teacher/ClassList/ClassListContainer.js
@@ -6,69 +6,7 @@ import courseApi from '../../../api/courses';
 import * as subscriptionTypes from '../../../constants/subscriptionTypes';
 import Feedback from '../../shared/FeedbackModal';
 import ClassList from './ClassList';
-
-const columns = [
-  {
-    defaults: require('../../../assets/images/placeholders/no-user.png'),
-    display: '',
-    key: 'avatarurlmedium',
-    type: 'string',
-  },
-  {
-    defaults: '',
-    display: 'Naam',
-    key: 'name',
-    type: 'string',
-  },
-  {
-    defaults: '',
-    display: 'Geboortedatum',
-    key: 'birthdate',
-    type: 'number',
-  },
-  {
-    defaults: '',
-    display: 'Leerjaar',
-    key: 'grade',
-    type: 'number',
-  },
-  {
-    defaults: '',
-    display: 'Naam ouder',
-    key: 'parent_name',
-    type: 'string',
-  },
-  {
-    defaults: '',
-    display: 'Contact ouder',
-    key: 'parent_contact',
-    type: 'string',
-  },
-  {
-    defaults: '',
-    display: 'Leerplatform username',
-    key: 'usernames_platform',
-    type: 'string',
-  },
-  {
-    defaults: '',
-    display: 'Leerplatform hint',
-    key: 'password_hint',
-    type: 'string',
-  },
-  {
-    defaults: '',
-    display: 'Scratch username',
-    key: 'scratchusername',
-    type: 'string',
-  },
-  {
-    defaults: '',
-    display: 'Scratch paswoord',
-    key: 'scratchpassword',
-    type: 'string',
-  },
-];
+import columns from './ClassListColumns';
 
 class ClassListContainer extends React.Component {
   // init state + bind functions
