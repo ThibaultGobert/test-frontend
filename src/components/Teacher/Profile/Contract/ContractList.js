c97034fef466c31322e565df0a743f93b6f43c3d
Maarten Raes <maarten.raes1@gmail.com>
2018-03-06 16:11:47 +0100

    Convert to table

--- src/components/Teacher/Profile/Contract/ContractList.js
+++ src/components/Teacher/Profile/Contract/ContractList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Message, Button, Grid } from 'semantic-ui-react';
+import { Message, Button, Table, Popup, Label, Icon } from 'semantic-ui-react';
 import { values, upperFirst } from 'lodash';
 import FileUpload from 'react-file-input-previews-base64';
 import generatePDFLink from '../../../../functions/generatePDFLink';
@@ -20,55 +20,73 @@ const ContractList = ({ contracts }) => {
         header="Download hier je contract"
         content="Van zodra je contract klaar is, kan je het hier downloaden en getekend terug uploaden. File download wordt niet ondersteund door Safari."
       />
-      {contractsArray.map(contract => {
-        const pdf = getActivePDFFile(contract);
-        return (
-          <div className="ContractList__Item">
-            <span className="ContractList__Item__Period">{contract.period}</span>
-            <div className="ContractList__Item__NestedItem">
-              <label>Locatie</label>
-              <span>{contract.location}</span>
-            </div>
-            <div className="ContractList__Item__NestedItem">
-              <label>Rol leerkracht</label>
-              <span>{upperFirst(contract.teacher_role)}</span>
-            </div>
-            <div className="ContractList__Item__NestedItem">
-              <label>Statuut</label>
-              <span>{upperFirst(contract.teacher_statute)}</span>
-            </div>
-            <div className="ContractList__Item__NestedItem">
-              <label>Contract</label>
-              <span>{pdf.file_name}</span>
-            </div>
-            <div className="ContractList__Item__Buttons">
-              <Grid columns={2}>
-                <Grid.Column className="ContractList__Item__Buttons__Download">
-                  <a
-                    id={contract.id}
-                    href={generatePDFLink(contract)}
-                    download={pdf.file_name}
-                  >
-                    <Button primary>Download</Button>
-                  </a>
-                </Grid.Column>
-                <Grid.Column className="ContractList__Item__Buttons__Upload" >
-                  { !contract.signed_ok &&
-                    <FileUpload
-                      labelText=""
-                      parentStyle={{ margin: 0 }}
-                      callbackFunction={(file) => contractUploadCallback(file, contract.id)}
-                      accept="application/pdf"
-                      textFieldComponent={<input placeholder="Selecteer een file" type="text" />}
-                      buttonComponent={<Button primary>Upload</Button>}
-                    />
-                  }
-                </Grid.Column>
-              </Grid>
-            </div>
-          </div>
-        );
-      })}
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Seizoen</Table.HeaderCell>
+            <Table.HeaderCell>Locatie</Table.HeaderCell>
+            <Table.HeaderCell>Rol leerkracht</Table.HeaderCell>
+            <Table.HeaderCell>Statuut</Table.HeaderCell>
+            <Table.HeaderCell>Download</Table.HeaderCell>
+            <Table.HeaderCell>Upload</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {contractsArray.map(contract => {
+            const pdf = getActivePDFFile(contract);
+            return (
+              <Table.Row>
+                <Table.Cell>
+                  <Label ribbon>{contract.period}</Label>
+                </Table.Cell>
+                <Table.Cell>
+                  {contract.location}
+                </Table.Cell>
+                <Table.Cell>
+                  {upperFirst(contract.teacher_role)}
+                </Table.Cell>
+                <Table.Cell>
+                  {upperFirst(contract.teacher_statute)}
+                </Table.Cell>
+                <Table.Cell>
+                  <span className="ContractList__Item__Buttons__Download">
+                    <a
+                      id={contract.id}
+                      href={generatePDFLink(contract)}
+                      download={pdf.file_name}
+                    >
+                      <Popup
+                        trigger={<Button primary>Download</Button>}
+                        content={pdf.file_name}
+                      />
+                    </a>
+                  </span>
+                </Table.Cell>
+                { !contract.signed_ok &&
+                  <Table.Cell>
+                    <span className="ContractList__Item__Buttons__Upload" >
+                      <FileUpload
+                        labelText=""
+                        parentStyle={{ margin: 0 }}
+                        callbackFunction={(file) => contractUploadCallback(file, contract.id)}
+                        accept="application/pdf"
+                        textFieldComponent={<input placeholder="Selecteer een file" type="text" />}
+                        buttonComponent={<Button primary>Upload</Button>}
+                      />
+                    </span>
+                  </Table.Cell>
+                }
+                { contract.signed_ok &&
+                  <Table.Cell positive>
+                    <Icon name="checkmark" />
+                      In orde!
+                  </Table.Cell>
+                }
+              </Table.Row>
+            );
+          })}
+        </Table.Body>
+      </Table>
     </div>
   );
 };
